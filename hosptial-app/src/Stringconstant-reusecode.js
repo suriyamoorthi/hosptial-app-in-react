9f857e6f4dd785d07c64bfbdc1f9a0399d9746e1
suriyamoorthi <97622569+suriyamoorthi@users.noreply.github.com>
2023-05-14 21:38:46 +0530

    strujh

--- hosptial-app/src/Stringconstant-reusecode.js
+++ hosptial-app/src/Stringconstant-reusecode.js
@@ -1,17 +1,17 @@
 
-export const API_URL_APPIONMENT = "http://localhost:3001/Appionment";
+// export const API_URL_APPIONMENT = "http://localhost:3001/Appionment";
 
-export const API_URL_Admin ="http://localhost:3001/adminappionment";
+// export const API_URL_Admin ="http://localhost:3001/adminappionment";
 
-export const AUTH_API_URL = "http://localhost:3001/LoginRegister";
+// export const AUTH_API_URL = "http://localhost:3001/LoginRegister";
 
-export const PROFILE_API_URL = "http://localhost:3001/profile";
+// export const PROFILE_API_URL = "http://localhost:3001/profile";
 
 
-// export const API_URL_APPIONMENT = "https://hosptial-app.onrender.com/Appionment";
+export const API_URL_APPIONMENT = "https://hosptial-app.onrender.com/Appionment";
 
-// export const API_URL_Admin ="https://hosptial-app.onrender.com/adminappionment";
+export const API_URL_Admin ="https://hosptial-app.onrender.com/adminappionment";
 
-// export const AUTH_API_URL = "https://hosptial-app.onrender.com/LoginRegister";
+export const AUTH_API_URL = "https://hosptial-app.onrender.com/LoginRegister";
 
-// export const PROFILE_API_URL = "https://hosptial-app.onrender.com/profile";
+export const PROFILE_API_URL = "https://hosptial-app.onrender.com/profile";
